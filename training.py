8ae6c472f9c1da14b6a0be302146d1e4759bc915
RasmusBrostroem <201706904@post.au.dk>
2021-12-09 23:22:53 +0000

    Reorginized the training script

--- training.py
+++ training.py
@@ -20,9 +20,9 @@ agent.to(device)
 pg.init()
 
 # Parameters
-generations = 10
-episodes_per_gen = 10000 # Episodes before new generation
-batch_size = 1 #Episodes before param update
+generations = 3
+episodes_per_gen = 1000 # Episodes before new generation
+batch_size = 10 #Episodes before param update
 learning_rate = 0.001 # Learning rate
 decay_rate = 0.99 # Weight decay for Adam optimizer
 
@@ -32,102 +32,89 @@ optimizer = optim.Adam(agent.parameters(), lr=learning_rate, weight_decay=decay_
 # Environment
 env = gym.make('ConnectFour-v0')
 
-s = env.reset()
-env.configurePlayer(random.choice([-1,1]))
-
 # Main training loop
 # for gen in range(generations):
 #     model_save_path = 'AgentParameters/agent_params'+gen+'.pkl'
 #     if os.path.isfile(model_save_path):
 #         print('Loading model parameters')
 
-win_rate = []
-losses = []
-games_final_rewards = []
-
-episode = 0
-show = False
 
 def play_game(env, agent, opponent = None, show_game = False):
-    pass
-
-while True:
     s = env.reset()
     env.configurePlayer(random.choice([-1,1]))
 
-    if keyboard.is_pressed("Esc"):
-            sys.exit()
-
-    if keyboard.is_pressed("t"):
-        show = True
-        
-    # Playing a game
     while True:
-        if show:
+        if show_game:
             env.render()
-    
-        if keyboard.is_pressed("n"):
-            show = False
-            pg.display.quit()
-    
-        if env.player == -1:
+
+        if env.player == -1 and opponent is None:
             choices = env.game.legal_cols()
             action = random.choice(choices)
+        elif env.player == -1 and opponent is not None:
+            action = opponent.select_action(s)
         else:
             action = agent.select_action(s)
 
         s, r, done, _ = env.step(action)
 
         if done:
-            if show:
+            if show_game:
                 env.render(r)
+                pg.display.quit()
                 
             agent.rewards.append(r)
             agent.calculate_rewards()
-            games_final_rewards.append(r)
-            break
+            return r
         elif env.player == 1:
             agent.rewards.append(r)
 
         env.configurePlayer(env.player * -1)
+
+def update_agent(agent, optimizer):
+    loss = []
+    for log_prob, reward in zip(agent.saved_log_probs, agent.rewards):
+        loss.append(-log_prob * reward)
     
-    if episode % batch_size == 0:
-        # # Output agent parameters
-        # for param in agent.parameters():
-        #     print(param.data)
-        #     break
+    loss = torch.stack(loss).mean()
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
 
-        loss = []
-        for log_prob, reward in zip(agent.saved_log_probs, agent.rewards):
-            loss.append((-log_prob+1) * reward)
-        
-        loss = torch.stack(loss).mean()
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    del agent.rewards[:]
+    del agent.saved_log_probs[:]
 
-        losses.append(loss.detach().numpy())
+    return loss.detach().numpy()
 
-        del agent.rewards[:]
-        del agent.saved_log_probs[:]
+def train_agent(env, agent, optimizer, generations, episodes_per_gen, batchsize, opponent = None, print_every = 1000, show_every = 1000):
+    losses = []
+    games_final_rewards = []
 
-        # Calculating win percentage
-        # wins = [game_r == 1 for game_r in games_final_rewards]
-        # win_rate.append(np.mean(wins))
+    for gen in range(1,generations+1):
 
-        # del games_final_rewards[:]
+        for ep in range(1,episodes_per_gen+1):
+            if keyboard.is_pressed("Esc"):
+                sys.exit()
 
-        #print(f'Reinforce ep {episode} done. Winrate: {np.mean(wins)}. Loss: {loss.detach().numpy()}')
+            if ep % show_every == 0:
+                final_reward = play_game(env, agent, opponent, True)
+            else:
+                final_reward = play_game(env, agent, opponent)
+            
+            games_final_rewards.append(final_reward)
 
-    if episode % 1000 == 0:
-        # Calculating win percentage
-        wins = [game_r == env.game.win for game_r in games_final_rewards]
-        win_rate.append(np.mean(wins))
-        illegals = [game_r == env.game.illegal for game_r in games_final_rewards]
+            if ep*gen % batchsize == 0:
+                loss = update_agent(agent, optimizer)
+                losses.append(loss)
+            
+            if ep % print_every == 0:
+                wins = [game_r == env.game.win for game_r in games_final_rewards]
+                illegals = [game_r == env.game.illegal for game_r in games_final_rewards]
 
-        print(f'Reinforce ep {episode} done. Winrate: {np.mean(wins)}. Illegal move rate: {np.mean(illegals)}. Average Loss: {np.mean(losses)}')
+                print(f'Reinforce ep {ep} in gen {gen} done. Winrate: {np.mean(wins)}. Illegal move rate: {np.mean(illegals)}. Average Loss: {np.mean(losses)}')
 
-        del games_final_rewards[:]
-        del losses[:]
+                del games_final_rewards[:]
+                del losses[:]
 
-    episode += 1
+
+if __name__ == "__main__":
+    train_agent(env, agent, optimizer, generations, episodes_per_gen, batch_size)
