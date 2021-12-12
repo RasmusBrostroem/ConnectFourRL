66887afb2adb21846c33260247949b0e14393576
RasmusBrostroem <201706904@post.au.dk>
2021-12-12 11:40:07 +0000

    Update HumanPlay.py
    Made the agent able to take illegal moves against human

--- HumanPlay.py
+++ HumanPlay.py
@@ -33,7 +33,7 @@ def play_game(env, agent, opponent = None, show_game = False):
                     elif i == 9:
                         action = random.choice(choices)
         else:
-            action = agent.select_action(s, choices)
+            action = agent.select_action(s, None)
 
         s, r, done, _ = env.step(action)
 
