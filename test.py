99b5b57fcd5c73f3214fb5e22c5ab0236cba26cd
RasmusBrostroem <201706904@post.au.dk>
2021-12-16 01:32:21 +0100

    Changed test.py to be able to watch agents play

---
+++ test.py
@@ -0,0 +1,14 @@
+from testModel import matchup
+from minimaxAgent import MinimaxAgent
+from agent import DirectPolicyAgent
+import torch
+
+model = DirectPolicyAgent("cpu")
+model.train(False)
+model = torch.load("AgentParameters/LastHopeBoi_gen_4.pth")
+# opponent = DirectPolicyAgent("cpu")
+# opponent.train(False)
+# opponent = torch.load("AgentParameters/AverageJoe_gen_14.pth")
+opponent = MinimaxAgent(max_depth=0)
+
+matchup(model, opponent, 4, True)
