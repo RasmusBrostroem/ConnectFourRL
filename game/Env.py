6f06954349fea1f890c0a00fd09b23bcc8801845
RasmusBrostroem <201706904@post.au.dk>
2022-11-19 11:09:51 +0100

    added docstrings

--- game/Env.py
+++ game/Env.py
@@ -32,7 +32,14 @@ class Env():
         '''
         self.game.restart()
 
-    def check_user_events(self):
+    def check_user_events(self) -> None:
+        """Function that checks user events
+
+        The following events are handled:
+            - If the user clicks escape, or quit, then closes pygame and script
+            - If the user clicks "x", then sets 'display_game' to true, and the game will be shown
+            - If the user clicks "z", then sets 'display_game' to false, and game will not be shown
+        """
         pg.init() # if we call pg.display.quit in self.game.close_board() then we also close pygame, so we cant use pg.event.get() after. Therefore, we have to init pg every time
         for event in pg.event.get():
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
@@ -46,15 +53,31 @@ class Env():
                 self.game.close_board()
 
 
-    def assign_rewards(self, col_choice: int) -> str:
-        '''
-        Returns the game state seen for the perspective for self.currentPlayer.
-        Game state is defined as one of the following:
-        "win_reward" - if col_choice lead to a win
-        "tie_reward" - if col_choice lead to a tie
-        "illegal_reward" - if col_choice is an illegal move
-        "not_ended_reward" - if col_choice didn't lead to the game ending
-        '''
+    def assign_rewards(self, col_choice: int) -> None:
+        """Function that assigns rewards to the players based on the players reward parameters
+        and assigns 'None' to the 'game_succes' list of the current player.
+
+        The logic of the assignemt is:
+            - If the last move by the current player was a winning move, 
+            then the currentplayer gets their win reward appended and we change the last reward
+            of the other player to be the loss reward
+            - If the last move by the current player lead to a tie, then the current player gets
+            the tie reward appended to their reward list and we change the last reward for the other
+            player to the the tie reward
+            - If the current player made an illegal move, then we append the illegal move reward
+            to the current player and we change the last reward of the other player to be the tie reward
+            - If the last move by the current player didn't lead to any of the above outcomes,
+            then the game is still going and we append the not ended reward (0) to the current player 
+
+        Args:
+            col_choice (int): The column choosen by the currentplayer in the last 'select_action' call
+
+        Note:
+            For this function to work, then all players must have a rewards list as an attribute,
+            and they also need to have 'win_reward', 'loss_reward', 'tie_reward', 'illegal_reward' and
+            'not_ended_reward' in the params dictionary. Furthermore, the players must have a 'game_succes' list also.
+        """
+        
         self.currentPlayer.game_succes.append(None)
         if self.game.winning_move():
             self.currentPlayer.rewards.append(self.currentPlayer.params["win_reward"])
@@ -81,12 +104,12 @@ class Env():
             self.currentPlayer.rewards.append(self.currentPlayer.params["not_ended_reward"])
     
     def step(self) -> bool:
-        '''
-        Takes a step in the environment by a player choosing an action
-        
-        Returns
-            - done: boolean that tells if the game is over or not
-        '''
+        """Takes a step in the environment by a player choosing an action, placing the player piece, 
+        and assigning rewards to the players.
+
+        Returns:
+            bool: That tells the environment if the game ended or not (true if ended, false if not)
+        """
 
         # Making current player select an action
         if self.allow_illegal_moves:
@@ -117,6 +140,15 @@ class Env():
             self.currentPlayer = self.player1
 
     def play_game(self) -> None:
+        """Function to play a full game by the two players.
+
+        The function selects a player at random to start, 
+        and takes a step in the invornment until the game has ended.
+
+        While the game is ongoing, then checks for user events, with 'check_user_events'.
+
+        Once the game has ended, then it makes the players calculate their rewards and exits.
+        """
         self.reset()
         self.currentPlayer = random.choice([self.player1, self.player2])
 
