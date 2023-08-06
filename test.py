bac793c23f225fd5c368c3a4ecc54e353f95a7b8
jbirkesteen <jbsteenstrup@post.au.dk>
2023-08-06 13:07:46 +0200

    Add first version of self-training script
    Also changed a few things in the test script (basically made a very simple benchmarking script to check the performance after self-training)

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from game.players import DirectPolicyAgent, Player, MinimaxAgent
+from game.players import DirectPolicyAgent, Player, MinimaxAgent, TDAgent
 from game.Env import Env
 from game.connectFour import connect_four
 from torch.distributions import Categorical
@@ -18,39 +18,33 @@ from time import sleep
 # opponent = MinimaxAgent(max_depth=0)
 
 #player1 = DirectPolicyAgent(_playerPiece = 21, _winReward = 31, _lossReward = -11, _tieReward = 0.25, _illegalReward = -5, _device = "cpu")# #_winReward = 1, _lossReward = -1, _tieReward = 0.5, _illegalReward = -5, _device = "cpu")
-player2 = MinimaxAgent(player_piece = -1, win_reward = 10, loss_reward = -20)
-player1 = MinimaxAgent(player_piece=1)
-
+#player2 = MinimaxAgent(player_piece = -1, win_reward = 10, loss_reward = -20)
+#player1 = MinimaxAgent(player_piece=1)
 # run = neptune.init_run(project="DLProject/ConnectFour")
 # player1.log_params(neptune_run=run)
 # player2.log_params(neptune_run=run)
 
 # optimizerPL2 = optim.RMSprop(player2.parameters(), lr=0.1, weight_decay=0.95)
 
-environment = Env(player1, player2, allow_illegal_moves = False)
-# game = connect_four()
-# game.place_piece(1, 1)
-# game.draw_board()
-# sleep(1)
-# game.remove_piece(1)
-# game.draw_board()
-# sleep(1)
 
-for i in range(1,2):
+player1 = TDAgent(player_piece=1)
+player1.load_network_weights("learned_weights/player1_selftrain.pt")
+player1.is_training = False
+player2 = TDAgent(player_piece=-1)
+player2.load_network_weights("learned_weights/player1_selftrain_11k.pt")
+player2.is_training = False
+#player2 = MinimaxAgent(player_piece=-1)
+#player2 = Player(player_piece=-1)
+environment = Env(player1, player2, allow_illegal_moves=False)
+
+for i in range(1, 101):
+    #environment.self_play()
     environment.play_game()
+    wins = player1.stats["wins"]
+    games = player1.stats["games"]
+    print(f"Winrate, TDtraining against  : {wins}/{games}({wins/games})")
     # if i % 20 == 0:
     #     player2.update_agent(optimizer=optimizerPL2)
     #     player1.update_agent()
     #     player1.log_stats(neptune_run=run)
     #     player2.log_stats(neptune_run=run)
-
-
-
-#print(torch.tensor(3) == torch.tensor([3]))
-
-
-
-# print("player1 rewards")
-# print(player1.rewards)
-# print("player2 rewards")
-# print(player2.rewards)
