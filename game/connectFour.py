b6831d8b1c9bbef9bd6fcaf8ff8765f2e0bc817e
jbirkesteen <70960888+jbirkesteen@users.noreply.github.com>
2023-08-22 13:20:45 +0200

    Linting updates

--- game/connectFour.py
+++ game/connectFour.py
@@ -1,47 +1,62 @@
 import pygame as pg
-import numpy as np 
+import numpy as np
+
 
 class connect_four():
-    def __init__(self, _size = 700, _rows = 6, _columns = 7):
+    def __init__(self, _size=700, _rows=6, _columns=7):
         # The attributes to display the game
         pg.init()
         self.size = _size
         self.rows = _rows
         self.columns = _columns
 
-        self.board = np.zeros((self.rows,self.columns))
+        self.board = np.zeros((self.rows, self.columns))
         self.screen = None
 
     def draw_board(self):
         '''
         Draws the current state of the board
         '''
-        black = (0,0,0)
-        blue = (0,0,255)
-        red = (255,0,0)
-        yellow = (255,255,0)
+        black = (0, 0, 0)
+        blue = (0, 0, 255)
+        red = (255, 0, 0)
+        yellow = (255, 255, 0)
 
-        square_size = min(np.ceil(self.size/self.columns),np.ceil(self.size/self.rows))
-        
+        square_size = min(np.ceil(self.size/self.columns),
+                          np.ceil(self.size/self.rows))
+
         if not self.screen:
-            self.screen = pg.display.set_mode((square_size * self.columns, square_size * (self.rows+1)))
+            self.screen = pg.display.set_mode((square_size * self.columns,
+                                               square_size * (self.rows+1)))
 
         radius = int(square_size/2-5)
-        flipped_board = np.flip(self.board,0)
+        flipped_board = np.flip(self.board, 0)
 
         # Draw the squares and pieces
         for r in range(self.rows):
             for c in range(self.columns):
-                pg.draw.rect(self.screen, blue, (c*square_size, r*square_size+square_size, square_size, square_size))
+                pg.draw.rect(self.screen, blue, (c*square_size,
+                                                 r*square_size+square_size,
+                                                 square_size,
+                                                 square_size))
                 circle_x_center = int(c*square_size+square_size/2)
                 circle_y_center = int(r*square_size+square_size/2+square_size)
                 if flipped_board[r][c] == 0:
-                    pg.draw.circle(self.screen, black, (circle_x_center, circle_y_center), radius)
+                    pg.draw.circle(self.screen,
+                                   black,
+                                   (circle_x_center, circle_y_center),
+                                   radius)
                 elif flipped_board[r][c] == 1:
-                    pg.draw.circle(self.screen, yellow, (circle_x_center, circle_y_center), radius)
+                    pg.draw.circle(self.screen,
+                                   yellow,
+                                   (circle_x_center, circle_y_center),
+                                   radius)
                 else:
-                    pg.draw.circle(self.screen, red, (circle_x_center, circle_y_center), radius)
-        
+                    pg.draw.circle(self.screen,
+                                   red,
+                                   (circle_x_center, circle_y_center),
+                                   radius)
+
         # Draw the result if there is one
         # if self.winning_move():
         #     text = f"Player {2 if self.player == -1 else self.player} won!"
@@ -113,11 +128,11 @@ class connect_four():
 
     def return_board(self) -> None:
         '''
-        Returns the current state of the board as a copy of the board, 
+        Returns the current state of the board as a copy of the board,
         such that no changes are made to the actual board.
         '''
         return np.copy(np.flip(self.board, 0))
-    
+
     def is_legal(self, column: int) -> bool:
         '''
         Checks for legal move returns a boolean
@@ -152,34 +167,34 @@ class connect_four():
         Checks if the board has a winner and returns boolean
         Returns true if player a player made the winning move and false if not
         '''
-        #Check horizontal locations for win
+        # Check horizontal locations for win
         for c in range(self.columns-3):
             for r in range(self.rows):
-                winning_sum = np.sum(self.board[r,c:c+4])
+                winning_sum = np.sum(self.board[r, c:c+4])
                 if winning_sum == 4 or winning_sum == -4:
                     return True
-        
-        #Check vertical locations for win
+
+        # Check vertical locations for win
         for c in range(self.columns):
             for r in range(self.rows-3):
-                winning_sum = np.sum(self.board[r:r+4,c]) 
+                winning_sum = np.sum(self.board[r:r+4, c])
                 if winning_sum == 4 or winning_sum == -4:
                     return True
-        
-        #Check diagonals for win
+
+        # Check diagonals for win
         for c in range(self.columns-3):
             for r in range(self.rows-3):
-                sub_matrix = self.board[r:r+4,c:c+4]
-                #diag1 is the negative slope diag
+                sub_matrix = self.board[r:r+4, c:c+4]
+                # diag1 is the negative slope diag
                 diag1 = sub_matrix.trace()
-                #diag2 is the positive slope diag
+                # diag2 is the positive slope diag
                 diag2 = np.fliplr(sub_matrix).trace()
-                
+
                 if diag1 == 4 or diag1 == -4 or diag2 == 4 or diag2 == -4:
                     return True
-        
+
         return False
-    
+
     def is_tie(self) -> bool:
         '''
         Checks if the game is a tie, returns a boolean
@@ -199,7 +214,7 @@ class connect_four():
         Restarts the game by setting all entries in board to 0
         '''
         self.board *= 0
-    
+
     def is_done(self, is_legal_move: bool) -> bool:
         '''
         Checks if the game is done and returns a boolean.
@@ -209,7 +224,7 @@ class connect_four():
             3. If the player makes an illegal move
         '''
         return not is_legal_move or self.winning_move() or self.is_tie()
-    
+
     # def evaluate(self, player: int, legal: bool):
     #     '''
     #     Evaluates current state and returns a reward. Note: only makes sense for player with id 1.
@@ -233,5 +248,3 @@ class connect_four():
     #         return self.tie
     #     else:
     #         return 0
-
-
